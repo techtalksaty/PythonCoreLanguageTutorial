3ab0f9bcc7cfed1e9e36f75520146d4f4de045f0
TechTalkSaty <noreply@example.com>
2019-09-29 01:03:59 +0530

    Added additional information on HelloWorld.py

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,1 +1,10 @@
+#This is the first python program of the series - Python Tutorials
+
+#This statement will print Hello World! on the console
+print("Hello World!")
+
+#This statement will print sum of 10 and 20 on the console
+print(10+20)
+
+#This statement will print the division of 20 by 4
 print(20/4)
