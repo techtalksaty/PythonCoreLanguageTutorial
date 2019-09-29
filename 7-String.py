563c0104a599be00d848b6cf8231fbf6d1dbaa5e
TechTalkSaty <noreply@example.com>
2019-09-29 18:24:14 +0530

    Added code to String.py

--- 7-String.py
+++ 7-String.py
@@ -5,18 +5,54 @@ double quotation marks e.g. name="MyName"
 tripple quotation marks e.g. name="""MyName"""
 '''
 
+
+greeting = 'Good Evening!'
+greeting1 = 'Good Evening!'
+greeting2 = "Good Evening!"
+greeting3 = '''Good Evening!'''
+greeting4 = """Good Evening!"""
+
+#To get the type of data
+print(type(greeting1))
+print(type(greeting2))
+print(type(greeting3))
+print(type(greeting4))
+
+#To get the actula data inside the variable
+print(greeting1)
+print(greeting2)
+print(greeting3)
+print(greeting4)
+
+
 #String literal can be displayed with the print() function
 
 #Assign String to a Variable e.g. a="any string"
+#assignment happence from right to left
 
-#Multiline Strings 
+greeting = 'Good Evening!'
+
+#Multiline Strings
+
+
+greeting = '''Good Evening!
+one more line!!!'''
+print(greeting)
+
 
 #Strings are Arrays
+print(greeting[0])
+print(greeting[1])
+print(greeting[2])
+print(greeting[3])
 
 #Slicing
-#Negative Indexing
-#String Length
+print(greeting[5:12])
 
+#Negative Indexing
+print(greeting[-8:-1])
+#String Length
+print("Length of the string is ", len(greeting))
 
 
 #String Methods
@@ -33,5 +69,24 @@ tripple quotation marks e.g. name="""MyName"""
 '''
 
 
+print(greeting.strip())
+print(greeting.lower())
+print(greeting.upper())
+print(greeting.replace("Good","Happy"))
+print(greeting.split(" "))
 
+x = "Go" not in greeting  
+print(x)
+
+g1="Good"
+g2="Evening"
+greeting =g1+" "+g2+"!"
+print(greeting)
+
+qty=3
+item=17
+price=35.75
+
+myOrder = "I want {0} pieces of item {1} for {2} rupees"
+print(myOrder.format(qty,item,price))
 
