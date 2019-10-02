f7b2daad9af9d2479a3112429c00430bbb355b12
TechTalkSaty <noreply@example.com>
2019-10-02 20:24:52 +0530

    File Added Operators.py

---
+++ 9-Operators.py
@@ -0,0 +1,101 @@
+#Operators are used to perform operations on variables and values.
+
+#Arithmetic operators
+'''
+Arithmetic operators are used with numeric values to perform 
+common mathematical operations
+
++ Addtion
+- Substraction
+* Multiplication
+/ Division
+% Modulus
+** Exponentiation
+// Floor Divison 
+
+x=9
+y=4
+
+#print(x+y)
+#print(x-y)
+#print(x*y)
+#print(x/y)
+#print(x%y)
+#print(x**y)
+#print(x/y)
+#print(x//y)
+'''
+
+#Assignment operators
+'''
+Assignment operators are used to assign values to variables
+
+num = 10
+name = "nameofemployee"
+print(num)
+
+num+=1 # num = num + 1
+print(num)
+
+num-=1 # num = num - 1
+print(num)
+'''
+
+#Comparison operators
+'''
+Comparison operators are used to compare two values
+== Equal To
+!= No Equal To
+>, >= Greater Than and Greater Than Equal To
+<, <= Less Than and Less Than Equal To
+x=10
+y=10
+print(x>=y)
+'''
+
+#Logical operators
+'''
+Logical operators are used to combine conditional statements
+
+and
+or
+not
+x=7
+print(not(x>5 and x<10))
+'''
+
+#Identity operators
+'''
+Identity operators are used to compare the objects, not if they are equal, 
+but if they are actually the same object, with the same memory location
+x=["name1","name2"]
+y=["age1","age2"]
+z=x
+
+#print(x is not y)
+#print(x is not z)
+
+name1="name"
+name2="name"
+
+print(name1 is name2)
+
+'''
+
+
+#Membership operators
+'''
+Membership operators are used to test if a sequence is presented in an object
+
+in
+not in
+
+'''
+
+
+#x = ["apple","banana"]
+#print("banana" not in x)
+
+name = "Satyarth rao"
+print("R" in name.upper())
+
