0f652abce86b6f0217b9918ac9ca92dc34090efe
TechTalkSaty <noreply@example.com>
2019-10-15 01:07:07 +0530

    Added List.py file

---
+++ 14-Lists.py
@@ -0,0 +1,82 @@
+'''
+A list is a collection of arbirary objects, somewhat like an arry 
+in other programming language but more flexible
+Lists are defined by opening closing square brackets []
+and values are sepetared by comma   
+'''
+#my_list = [] #Empty List
+#print(my_list)
+
+
+#print(my_list)
+#print(my_list[3])
+#print(my_list[-3])
+#print(my_list[2:6])
+#print(my_list[-6:-2])
+#my_list[5]=20.90
+#print(my_list)
+
+'''
+for item in my_list:
+    print(item)
+'''
+
+'''
+if 20 in my_list: 
+    print("20 is in the list")
+else:
+    print("20 is not faound in the list")    
+'''
+
+#print(len(my_list))
+
+#my_list.append("New Item")
+#my_list.append("New Item2")
+#print(my_list)
+
+
+#my_list.insert(4,"New Item")
+#print(my_list)
+
+# removed_value  = my_list.remove('Name1')
+# print(my_list)
+# print(removed_value)
+
+# poped_item = my_list.pop(4)
+# print(my_list)
+# print(poped_item)
+
+
+#del my_list[1]
+#print(my_list)
+
+#my_list.clear()
+#print(my_list)
+
+#my_new_list = list(my_list)
+
+#print(my_list)
+#print(my_new_list)
+
+#my_list1 = ['Name1',"Name2",'Name3',"Name4"]
+#my_list2 = [1,2,3,12.45,True]
+
+#my_list3 = my_list1+my_list2
+#print(my_list3)
+
+
+y = 'hello world',
+print(type(y))
+
+''' Properties of Lists
+List are ordered
+List can contain any arbitrary object
+Lists can be accessed by index
+List can be nested to an arbitrary depth
+Lists are mutable
+Lists are dynamic  
+'''
+
+
+
+
